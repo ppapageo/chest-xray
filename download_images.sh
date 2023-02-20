fa014a90b172493af1b106e03b16694c602bd60c
Panagiotis Papageorgiou <34631960+ppapageo@users.noreply.github.com>
2023-02-20 21:03:44 +0200

    Add download script (#1)
    * Create image_urls.txt
    
    * Create download_images.sh

---
+++ download_images.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+filename="$1"
+it=0
+while read -r line
+do
+	if [ "$line" ] # skip blank lines
+  then
+		export URL=`echo $line`
+		export NAME=images-${it}.tar.gz
+		echo $NAME
+		echo "Running wget -c ${NAME} $URL --continue"
+		wget -O ${NAME} $URL --continue
+		it=$((${it}+1));
+	fi
+done < "$filename"
+
+echo "DONE"
