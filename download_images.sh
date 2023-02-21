00d5122d9d83176afb827f1cc71f226894701788
Panagiotis Papageorgiou <34631960+ppapageo@users.noreply.github.com>
2023-02-21 10:42:24 +0200

    Update download_images.sh

--- download_images.sh
+++ download_images.sh
@@ -4,12 +4,13 @@ it=0
 while read -r line
 do
 	if [ "$line" ] # skip blank lines
-  then
+  	then
 		export URL=`echo $line`
 		export NAME=images-${it}.tar.gz
 		echo $NAME
 		echo "Running wget -c ${NAME} $URL --continue"
 		wget -O ${NAME} $URL --continue
+		tar -xvzf ${NAME} 
 		it=$((${it}+1));
 	fi
 done < "$filename"
